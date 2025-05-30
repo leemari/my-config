6de757f72661bc5808b899906628231c7dd484a9
Richard Leeman <richard@leemanfamily.me.uk>
2025-05-30 20:55:05 +0100

    Added ZIG

--- mac.sh
+++ mac.sh
@@ -14,3 +14,16 @@ xcrun -f lldb-dap
 export PATH="$PATH:<location of lldb-dap from previous command>"
 
 brew install clang-format
+
+brew install zig
+
+Install ZIG LSP, i.e. ZLS
+
+# change the below version to match your ZIG version or the latest available which you can get here https://zigtools.org/zls/install/?zig_version=0.14.1&compatibility=full
+git clone https://github.com/zigtools/zls
+cd zls
+git checkout 0.14.0
+zig build -Doptimize=ReleaseSafe
+
+# Add below to your path
+~/myapps/zls/zig-out/bin
